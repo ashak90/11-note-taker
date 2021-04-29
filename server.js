efa9a32041401e65c4a373c24216581ae1711b45
Asha Kumar <ashakumar90@gmail.com>
2021-04-29 15:14:23 -0400

    added the app.get function to load the index and notes pages

--- server.js
+++ server.js
@@ -1,3 +1,25 @@
-const express = require("express")
-
 console.log("Welcome to Note Taker App")
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+var PORT = process.env.PORT || 5500;
+let database = require('./db/db.json')
+
+app.use(express.urlencoded({extended:true})); 
+app.use(express.json());
+
+app.get("/notes",function(req, res){
+    res.sendFile(path.join(__dirname,"./public/notes.html"));
+})
+
+
+
+app.get("*", function(req,res) {
+    res.sendFile(path.join(__dirname,"./public/index.html"))
+})
+
+app.listen(PORT, ()=> {
+    console.log(`Listening on port ${PORT}`)
+});
